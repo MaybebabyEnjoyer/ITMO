3c62cc56e25959eb06a3ed65f904ef0edbd58cc4
Tiulenev Vadim <75691952+imperator30000@users.noreply.github.com>
2024-05-29 21:26:52 +0300

    Add files via upload

---
+++ course2/sem4/metopt/lab3/utils.py
@@ -0,0 +1,31 @@
+import time
+import tracemalloc
+
+
+def print_loss_graphic(plt, name, loss_history, duration, memory_used):
+    label = f'{name} (Time: {duration:.5f}s, Mem: {memory_used / 10 ** 6:.3f}MB)'
+    plt.plot(loss_history, label=label)
+    print(label)
+
+
+def solve_by_name_by_solver(plt, name, solver):
+    print_loss_graphic(plt, name, *solver())
+
+
+def time_decorator(fun):
+    def time_wrapper(*args, **kwargs):
+        start_time = time.time()  # Начинаем отслеживать время
+        return fun(*args, **kwargs), time.time() - start_time
+
+    return time_wrapper
+
+
+def memory_decorator(fun):
+    def memory_wrapper(*args, **kwargs):
+        tracemalloc.start()  # Начинаем отслеживать память
+        res = fun(*args, **kwargs)
+        _, peak = tracemalloc.get_traced_memory()  # Пиковое использование памяти
+        tracemalloc.stop()  # Остановка отслеживания памяти
+        return *res, peak
+
+    return memory_wrapper
